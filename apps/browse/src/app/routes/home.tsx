05e39fdcc6bef592d25d9735213f60722cc6fe85
Andrii <fedakandrew@gmail.com>
2025-05-10 23:01:47 +0300

    fixed

--- apps/browse/src/app/routes/home.tsx
+++ apps/browse/src/app/routes/home.tsx
@@ -1,16 +1,6 @@
-import { useState } from 'react';
-import Button from '@mui/material/Button';
-
 import { MainPage } from '@src/pages/main';
 
 // eslint-disable-next-line import/no-default-export
 export default function App() {
-  const [name, setName] = useState('asdf');
-  return (
-    <>
-      <p className="text-3xl font-bold underline">Hello world! {name}</p>
-      <MainPage />
-      <Button variant="contained">Hello world</Button>
-    </>
-  );
+  return <MainPage />;
 }
--- apps/browse/src/pages/main/ui/Page/Page.tsx
+++ apps/browse/src/pages/main/ui/Page/Page.tsx
@@ -1,8 +1,24 @@
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+
 import { useGetServicesQuery } from '@src/entities/service';
 
 export function MainPage() {
   const { data } = useGetServicesQuery();
   console.log('Main page', data);
 
-  return <div>Main page {data?.join(',')}</div>;
+  return (
+    <Grid container spacing={2}>
+      <Grid size={4}>
+        <Stack spacing={2}>
+          <div>Column 1 - Row 1</div>
+          <div>Column 1 - Row 2</div>
+          <div>Column 1 - Row 3</div>
+        </Stack>
+      </Grid>
+      <Grid size={8}>
+        <div>Column 2</div>
+      </Grid>
+    </Grid>
+  )
 }
